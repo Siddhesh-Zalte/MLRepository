19503136d29dcd2504352ee944a808d06a6006e6
Siddhesh-Zalte <zaltesiddheshlsg@gmail.com>
2025-09-23 13:39:20 +0530

    Model Trainer done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,7 +5,7 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass 
-
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
@@ -44,4 +44,6 @@ if __name__=="__main__":
     train_data, test_data=obj.initiate_data_ingestion()
 
     data_tranformation=DataTransformation()
-    data_tranformation.initiate_data_transformation(train_data, test_data)
+    train_arr ,test_arr= data_tranformation.initiate_data_transformation(train_data, test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
